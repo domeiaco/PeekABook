694f2d819c6c62a5997b653ac2a3667c4d68d0f4
CiroDeLuca00 <c.deluca36@studenti.unisa.it>
2024-06-05 20:01:58 +0200

    Update Autore.java e Genere.java

--- Project/src/main/java/model/Autore.java
+++ Project/src/main/java/model/Autore.java
@@ -1,5 +1,42 @@
 package model;
 
-public class Autore {
+import java.util.List;
 
+public class Autore{
+	private int codice;
+	private String nome;
+	private String cognome;
+	private List<Libro> libri;
+	
+	public int getCodice() {
+		return codice;
+	}
+	
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public List<Libro> getLibri(){
+		return libri;
+	}
+	
+	public void setLibri(List<Libro> libri) {
+		this.libri = libri;
+	}
 }
--- Project/src/main/java/model/Genere.java
+++ Project/src/main/java/model/Genere.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Genere {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Genere{
+	private String nome;
+	private List<Libro> libri;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<Libro> getLibri(){
+		return libri;
+	}
+	
+	public void setLibri(List<Libro> libri) {
+		this.libri = libri;
+	}
 }
