6f65adf7b04be95f494d64e772f6ba6f5d043cee
domeiaco <135246700+domeiaco@users.noreply.github.com>
2024-06-10 17:58:46 +0200

    DAO

--- Project/src/main/java/model/CarrelloDAO.java
+++ Project/src/main/java/model/CarrelloDAO.java
@@ -1,5 +1,40 @@
 package model;
 
-public class CarrelloDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
+public class CarrelloDAO {
+	public Carrello doRetrieveCarrelloByUtente(Utente u) {
+		return null;
+	}
+	
+	public int doUpdateCarrello(Carrello carrello) {
+		return 1;
+	}
+	
+	public int doCreateCarrello(Utente u) {
+		try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Carrello(utente,totale,numeroArticoli) VALUES (?,?,?)");
+            ps.setDouble(2, 0);
+            ps.setInt(1, u.getId());
+            ps.setInt(3, 0);
+            
+            int x = ps.executeUpdate();
+
+            return x>0 ? 1:0;
+
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+	}
+	
+	public int doClearCarrello(Utente u) {
+		return 1;
+	}
 }
--- Project/src/main/java/model/UtenteDAO.java
+++ Project/src/main/java/model/UtenteDAO.java
@@ -1,5 +1,226 @@
 package model;
 
-public class UtenteDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UtenteDAO {
+	public int doRetrieveIdByUsername(String user) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("SELECT id FROM Utente WHERE username=?");
+			ps.setString(1, user);
+			
+			ResultSet rs = ps.executeQuery();
+			int id=-1;
+			if(rs.next()) {
+				id = rs.getInt("id");
+			}
+			return id;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Utente doRetrieveByUsernamePassword(String username, String password) {
+		Utente u = new Utente();
+		u.setPassword(password);
+		String pass= u.getPassword();
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente WHERE username=? AND passwordHash=?");
+			ps.setString(1, username);
+			ps.setString(2, pass);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				u.setId(rs.getInt(1));
+				u.setAdmin(rs.getInt(2));
+				u.setEmail(rs.getString(3));
+				u.setUsername(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				u.setNome(rs.getString(6));
+				u.setCognome(rs.getString(7));
+				u.setVia(rs.getString(8));
+				u.setCivico(rs.getInt(9));
+				u.setCitta(rs.getString(10));
+				u.setCap(rs.getInt(11));
+			}
+			else {
+				return null;
+			}
+			
+			ps= con.prepareStatement("SELECT * FROM Carrello WHERE utente=?");
+			ps.setInt(1, u.getId());
+			rs = ps.executeQuery();
+			Carrello c = new Carrello();
+			c.setUtente(u);
+			if(rs.next()) {
+				c.setTotale(rs.getDouble("totale"));
+				c.setNumeroArticoli(rs.getInt("numeroArticoli"));
+			}
+			
+			return null;
+			////////////////// ?
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public List<Utente> doRetrieveAllUtenti(Utente adm, int limit, int offset){
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente WHERE id!=? LIMIT ?,?");
+			ps.setInt(1, adm.getId());
+			ps.setInt(2, limit);
+			ps.setInt(3, offset);
+			ResultSet rs = ps.executeQuery();
+			ArrayList<Utente> utenti= new ArrayList<>();
+			Utente u;
+			
+			while(rs.next()) {
+				u=new Utente();
+				u.setId(rs.getInt(1));
+				u.setAdmin(rs.getInt(2));
+				u.setEmail(rs.getString(3));
+				u.setUsername(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				u.setNome(rs.getString(6));
+				u.setCognome(rs.getString(7));
+				u.setVia(rs.getString(8));
+				u.setCivico(rs.getInt(9));
+				u.setCitta(rs.getString(10));
+				u.setCap(rs.getInt(11));
+				utenti.add(u);
+			}
+			return utenti;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public int doSaveUtente(Utente u) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("INSERT INTO Utente(adm,email,username,passwordHash, nome, cognome, via, civico, citta, CAP) VALUES(?,?,?,?,?,?,?,?,?,?)");
+			ps.setInt(1, 0);
+			ps.setString(2, u.getEmail());
+			ps.setString(3, u.getUsername());
+			ps.setString(4, u.getPassword());
+			ps.setString(5, u.getNome());
+			ps.setString(6, u.getCognome());
+			ps.setString(7, u.getVia());
+			ps.setInt(8, u.getCivico());
+			ps.setString(9, u.getCitta());
+			ps.setInt(10, u.getCap());
+			
+			int rows=ps.executeUpdate();
+			return rows>0 ? 1:0;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public int doDeleteUtente(Utente u) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("DELETE FROM Utente WHERE id=?");
+			ps.setInt(1, u.getId());
+			
+			int rows= ps.executeUpdate();
+			return rows>0 ? 1:0;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public int doUpdateUtente(Utente u){
+        try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("UPDATE Utente SET nome=?, cognome=?, via=?, civico=?, citta=?, cap=?, email=? WHERE id=?");
+
+            ps.setString(1, u.getNome());
+            ps.setString(2, u.getCognome());
+            ps.setString(3, u.getVia());
+            ps.setInt(4, u.getCivico());
+            ps.setString(5, u.getCitta());
+            ps.setInt(6, u.getCap());
+            ps.setString(7, u.getEmail());
+            ps.setInt(8, u.getId());
+
+            int rows= ps.executeUpdate();
+
+            return rows>0 ? 1:0;
+
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+	
+	public int doSetAdmin(Utente u) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("UPDATE Utente SET adm=? WHERE id=?");
+			ps.setInt(1, 1);
+			ps.setInt(2, u.getId());
+			int rows=ps.executeUpdate();
+			return rows>0 ? 1:0;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public int doRemoveAdmin(Utente u) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("UPDATE Utente SET adm=? WHERE id=?");
+			ps.setInt(1, 0);
+			ps.setInt(2, u.getId());
+			int rows=ps.executeUpdate();
+			return rows>0 ? 1:0;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public boolean doVerifyDuplicate(Utente utente) {
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("SELECT id FROM Utente WHERE username=?");
+			ps.setString(1, utente.getUsername());
+			ResultSet rs =ps.executeQuery();
+			if(rs.next())
+				return true;
+			else
+				return false;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Utente doRetrieveById(Integer id) {
+		Utente u = new Utente();
+
+		try(Connection con = ConPool.getConnection()){
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente WHERE id=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				u.setId(rs.getInt(1));
+				u.setAdmin(rs.getInt(2));
+				u.setEmail(rs.getString(3));
+				u.setUsername(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				u.setNome(rs.getString(6));
+				u.setCognome(rs.getString(7));
+				u.setVia(rs.getString(8));
+				u.setCivico(rs.getInt(9));
+				u.setCitta(rs.getString(10));
+				u.setCap(rs.getInt(11));
+				return u;
+			}
+			else {
+				return null;
+			}
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
